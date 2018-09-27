53961592c4c9fc9bf976716d77adf0f2f30833ea
Cazajous Miguel A <migue7490@gmail.com>
2018-09-27 10:30:08 +0000

    Algunas modificaciones

--- pruebas/Sensores/sensores.py
+++ pruebas/Sensores/sensores.py
@@ -1,14 +1,21 @@
-import random
+import random, multiprocesing
 
-def bateria():
+class sensor(multiprocesing.process):
+    def __init__(self, list):
+        multiprocesing.process.__init__(self)
+        self.list=list
+    def run(self):
+        while 1:
+            sleep(randint(0,5))
 
-def altura():
 
+def sensores():
+    sleep(randint(0,5))
+    return
 
 
 def main():
-    while(1):
-        print (random.randint(0,10))
+    sensores[]
 
 
 if __name__ == '__main__':
