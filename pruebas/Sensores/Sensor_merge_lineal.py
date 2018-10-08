1aa722bb10860e71d483769b96122ad0361d93cd
Cazajous Miguel A <migue7490@gmail.com>
2018-10-08 11:03:03 -0300

    Dos nuevos archivos sensor.py para probar sobrecarga

---
+++ pruebas/Sensores/Sensor_merge_lineal.py
@@ -0,0 +1,20 @@
+import random
+import time
+import MergeSort
+from multiprocessing import Process
+
+N = 100
+
+class Sensor(Process):
+    def __init__(self, obj, indice_lista):
+        super(Sensor, self).__init__()
+        self.obj=obj
+        self.indice_lista=indice_lista
+
+    def run(self):
+        while 1:
+            codigo_alarma = random.randint(0,10)
+            #time.sleep(random.randint(10,12))
+            MergeSort.mergesort([random.randint(0,N) for i in range(N)])
+            self.obj.set_value(self.indice_lista, codigo_alarma)
+            print("Soy Sensor - Alarma - Lista", self.indice_lista, codigo_alarma, self.obj.get_obj())
