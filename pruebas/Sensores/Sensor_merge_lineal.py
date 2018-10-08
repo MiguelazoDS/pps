241013b0c114b4fa9a3f08a6d1fd8e98c3ca4d3f
Cazajous Miguel A <migue7490@gmail.com>
2018-10-08 11:19:37 -0300

    Modificaciones en sensores con mergesort

--- pruebas/Sensores/Sensor_merge_lineal.py
+++ pruebas/Sensores/Sensor_merge_lineal.py
@@ -3,18 +3,18 @@ import time
 import MergeSort
 from multiprocessing import Process
 
-N = 100
 
 class Sensor(Process):
-    def __init__(self, obj, indice_lista):
+    def __init__(self, obj, indice_lista,tamano):
         super(Sensor, self).__init__()
         self.obj=obj
         self.indice_lista=indice_lista
+        self.tamano=tamano
 
     def run(self):
         while 1:
             codigo_alarma = random.randint(0,10)
-            #time.sleep(random.randint(10,12))
-            MergeSort.mergesort([random.randint(0,N) for i in range(N)])
-            self.obj.set_value(self.indice_lista, codigo_alarma)
+            MergeSort.mergesort([random.randint(0,tamano) for i in range(tamano)])
+            time = time.time()
+            self.obj.set_value(self.indice_lista, time)
             print("Soy Sensor - Alarma - Lista", self.indice_lista, codigo_alarma, self.obj.get_obj())
--- pruebas/Sensores/Sensor_merge_paralelo.py
+++ pruebas/Sensores/Sensor_merge_paralelo.py
@@ -3,18 +3,19 @@ import time
 import MergeSort
 from multiprocessing import Process
 
-N = 100
 
 class Sensor(Process):
-    def __init__(self, obj, indice_lista):
+    def __init__(self, obj, indice_lista,tamano):
         super(Sensor, self).__init__()
         self.obj=obj
         self.indice_lista=indice_lista
+        self.tamano=tamano
 
     def run(self):
         while 1:
             codigo_alarma = random.randint(0,10)
-            #time.sleep(random.randint(10,12))
-            MergeSort.mergesort_paralelo([random.randint(0,N) for i in range(N)])
+            MergeSort.mergesort_paralelo([random.randint(0,tamano) for i in range(tamano)])
+            time = time.time()
+            self.obj.set_value(self.indice_lista, time)
             self.obj.set_value(self.indice_lista, codigo_alarma)
             print("Soy Sensor - Alarma - Lista", self.indice_lista, codigo_alarma, self.obj.get_obj())
