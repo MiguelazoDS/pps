38bdc261e1cd035fdce7d04d760464999753cc4a
Cazajous Miguel A <migue7490@gmail.com>
2018-10-11 13:25:10 -0300

    Script en bash que guarda ejecuciones en un archivo y algunas modificaciones en planificador merge

--- pruebas/Sensores/merge/planificador_merge_paralelo.py
+++ pruebas/Sensores/merge/planificador_merge_paralelo.py
@@ -1,60 +1,78 @@
 import Sensor_merge_paralelo
 import random
 import time
+import numpy
+import sys
 from multiprocessing import Process, Manager, Lock
 from multiprocessing.managers import BaseManager
 
 
-def alarmas(object, num_process):
-
-    while 1:
-        #time.sleep(random.randint(15,16))
+def alarmas(object, num_process, tiempo_limite):
+    tiempo_inicio = time.time()
+    while (1):
         lista = object.get_obj()
-        #print("Vigilante " , lista)
         for i in range (0,num_process):
-            if(lista[i]>0):
+            if(lista[i] > 0):
                 tiempo = time.time() - lista[i]
-                object.set_value(i,-1)
-                print (tiempo)
-
-
+                object.set_value(i, -1)
+                object.set_value_tiempos(tiempo)
 
 class ListObj(object):
-        def __init__(self, lista):
+        def __init__(self, lista, lista_tiempos_promedios):
                 self.lista = lista
+                self.lista_tiempos_promedios = lista_tiempos_promedios
 
-        def set_value(self, indice_lista, codigo_alarma):
-                self.lista[indice_lista] = codigo_alarma
+        def set_value(self, indice_lista, tiempo_atencion):
+                self.lista[indice_lista] = tiempo_atencion
+
+        def set_value_tiempos(self, tiempo_atencion):  #para estadistica
+                self.lista_tiempos_promedios.append(tiempo_atencion)
 
         def get_obj(self):
                 return self.lista
 
+        def get_value_tiempos(self): #para estadistica
+                return self.lista_tiempos_promedios
 
 if __name__=="__main__":
+        num_process = 15
+        tamano_de_matriz = 10000
+        if len(sys.argv) >2:
+            temp1=int(sys.argv[2])
+            if sys.argv[2].isdigit() and temp1 > 10000 and temp1 < 1000000:
+                tamano_de_matriz=temp1#cantidad de valores
+        if len(sys.argv) >1:
+            temp2=int(sys.argv[1])
+            if sys.argv[1].isdigit() and temp2 > 15 and temp2 < 100:
+                num_process = temp2 #cantidad de sensores
 
         BaseManager.register('ListObj', ListObj)
         manager = BaseManager()
         manager.start()
 
-        num_process = 15 #cantidad de senores
-        tamano = 10000
-
+        tiempo_limite = 5 #tiempo que corre alarmas
         lista=list(range(num_process))
         lista = [-1 for i in range(num_process)] #inicializamos con -1
-        listObl = manager.ListObj(lista)
-
-        print(listObl.get_obj())
+        lista_tiempos_promedios = []
+        listObl = manager.ListObj(lista,lista_tiempos_promedios)
 
         process_list = []
         for p in range(num_process):
-                proc = Sensor_merge_paralelo.Sensor(listObl, p, tamano)
+                proc = Sensor_merge_paralelo.Sensor(listObl, p, tamano_de_matriz)
                 process_list.append(proc)
-
 
         for x in range(num_process):
                 process_list[x].start()
 
-        alarmas(listObl,num_process).start()
+        alarmas= Process(target=alarmas, args=(listObl,num_process, tiempo_limite))
+        alarmas.start()
+
+        time.sleep(tiempo_limite)
 
         for x in range(num_process):
-                process_list[x].join()
+                process_list[x].terminate()
+
+        alarmas.terminate()
+
+        media = numpy.mean(listObl.get_value_tiempos() ) #promedio de ti
+        print(media)
