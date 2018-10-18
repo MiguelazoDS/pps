90385239d5459dcd19a6d2956a70a7b776b45281
Cazajous Miguel A <migue7490@gmail.com>
2018-10-18 10:42:48 -0300

    Recuperando archivos

---
+++ pruebas/Sensores/matrices/process_queue_3.py
@@ -0,0 +1,79 @@
+import random
+import sys
+import time
+from multiprocessing import Process, Queue
+random.seed()
+
+#crea una lista con valores entre 0 y 10 (enteros)
+#devuelve la lista de enteros
+def genList (size):
+    randomList = []
+    for i in range(size):
+        randomList.append(random.randint(0,10))
+    return randomList
+
+#calcula la suma acumulada de todos lo valores de la lista
+def sumList(inList):
+    finalSum = 0
+    for value in inList:
+        finalSum = finalSum + value
+    return finalSum
+
+#crea una lista, suma los valores de la lista y coloca el resultado en una cola
+def doWork(N,q):
+    #crea una lista de enteros aleatorios
+    myList = genList (N)
+    #suma los valores de la lista generadas
+    finalSum = sumList(myList)
+    # guarda el resultado en la cola (Compartida por demas procesos)
+    q.put(finalSum)
+
+def main(tamano):
+
+    N = int(tamano) #argumento convertido en entero
+    startTime = time.time() #tiempo de inicio
+    q = Queue() # cola para compartir resultados
+
+    #defino cantidad de procesos que se corren
+    num_process = 4
+
+    # creo nombres para procesos
+    nameProcess = []
+    for i in range(0,num_process):
+      nameProcess.append("p" + str(i+1)) #arranca de 1 a 4
+
+    lenProcess = len(nameProcess)
+
+    # for i in range(0,lenProcess):
+    # print (nameProcess[i])
+
+    # defino procesos
+    for i in range(0,lenProcess):
+      nameProcess[i]= Process(target=doWork, args=(N//num_process,q))
+      nameProcess[i].start()
+
+    results = []
+    # reservo lugares en el array uno por cada proceso
+    for i in range(0,lenProcess):
+      #set block=True para bloquea la asignacion hasta que el resultado este disponible en la cola
+      results.append(q.get(True))
+
+    #sum the partial results to get the final result
+    finalSum = sumList(results)
+
+    #join para cada proceso
+    for i in range(0,lenProcess):
+      nameProcess[i].join()
+
+    endTime = time.time() #obtener el tiempo de finalizado
+    workTime =  endTime - startTime #calculo del tiempo para completar el trabajo
+
+    #imprimo resultados
+#    print ("Tiempo total " + str(workTime) + " segundos")
+#    print ("Suma del resultado final: " + str(finalSum) )
+
+
+if __name__ == '__main__':
+
+     if len(sys.argv) == 2 and sys.argv[1].isdigit():
+         main(sys.argv[1])
---
+++ pruebas/opencv/show_image.py
@@ -0,0 +1,8 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('1.jpg',0)
+
+plt.plot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.show()
